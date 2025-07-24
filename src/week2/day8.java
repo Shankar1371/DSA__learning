0ce1877037485ce50c619cefef3b693ab05426e9
shankarpunati <shankar.uma1371@gmail.com>
2025-07-23 19:02:18 -0500

    i am learning the programming and done with checking weather the given string is a palindrome or not

--- src/week2/day8.java
+++ src/week2/day8.java
@@ -1,10 +1,35 @@
 package week2;
 
+import java.util.Scanner;
+
 public class day8 {
     public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
         //lets go with the reversing a string
         String name="shankar";
         StringBuilder rev=new StringBuilder(name).reverse();
         System.out.println("reversed="+rev);
+        //check weather the give name is palindrome
+        //for example of palindrome is the word when reversed sounds the same
+        //like madam rev madam
+        System.out.println("Enter the name to check the palindrome=");
+        String pali=sc.nextLine();
+        //now we got the name you can just reverse then name and store that to find that weather the given name is
+        System.out.println("palindrome="+palindrome(pali));
+
+    }
+
+    private static boolean palindrome(String pali) {
+        //this checks weather the name is palindrome or not
+        int left=0;
+        int right=pali.length()-1;
+        //here we iterate a loop till we get the palindrome statement
+        while(left<right){
+            if(pali.charAt(left)!=pali.charAt(right)){
+                return false;
+            }
+            left++;right--;
+        }
+        return true;
     }
 }
