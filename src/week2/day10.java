f25b2e04321dacf0d79ae510bb19c4f74c93d752
shankarpunati <shankar.uma1371@gmail.com>
2025-07-28 15:44:40 -0500

    Learnt and did my first program in Hashmap concept and did the second program on it to find a duplicate element in an array using a Hashset

--- src/week2/day10.java
+++ src/week2/day10.java
@@ -1,6 +1,7 @@
 package week2;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class day10 {
     public static void main(String[] args){
@@ -15,5 +16,18 @@ public class day10 {
             //here we take iterate and when we get the value we just add it to the given number that we got from before
         }
         System.out.println("Character Frequencies:"+ freq);
+
+        //we need to find a duplicate Array in hashset
+        //firstly taking an array as an input
+        int arr[]={1,2,3,4,2,5};
+        //now lets create a hash
+        HashSet<Integer> chusa=new HashSet<>();
+        //now after creating a for loop
+        for(int i:arr){
+            //this will iterate
+            if(!chusa.add(i)){//this will find the number weather there in the arr if there is a duplicate it will let pass
+                System.out.println("the duplicate pound="+ i);
+            }
+        }
     }
 }
