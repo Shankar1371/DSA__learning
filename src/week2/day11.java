77a34b0cdbc47fa4a22cd645932208f6d4ce30bf
shankarpunati <shankar.uma1371@gmail.com>
2025-08-02 17:14:08 -0500

    learn how to use hashmap entryset() function which helped me to complete the easy program to count the word frequency in a sentence

--- src/week2/day11.java
+++ src/week2/day11.java
@@ -1,6 +1,8 @@
 package week2;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class day11 {
@@ -22,5 +24,22 @@ public class day11 {
             //for loop used here to move through the HashMap
             System.out.println(entry.getKey() + ":" + entry.getValue());
         }
+
+        //group the words that have same first starting letter
+        //we are using Map<character,list<String>>// for this
+        //lets take an array of words to work on
+        String[] ver = {"apple", "ant", "banana", "bat", "cat"};
+        //now we need to create a hash map to store what is similar starting
+        HashMap<Character, List<String>> simi=new HashMap<>();
+        //now we need to iterate through the string array to get the element
+        for(String s:ver){
+            //this will iterate now we need to compare this
+            //but before that we need to divide the character of the word
+            char c=s.charAt(0);
+            //sometimes this simi can be null so what should we do for that
+            simi.putIfAbsent(c,new ArrayList<>());
+            simi.get(c).add(s);
+        }
+        System.out.println(simi);
     }
 }
