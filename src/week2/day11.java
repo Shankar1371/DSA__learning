12f566f6c85b3113949853bec19b14f5067ad843
shankarpunati <shankar.uma1371@gmail.com>
2025-08-02 23:15:16 -0500

    //Given an array of strings strs, group the anagrams together. You can return the answer in any order. worked on it and got the solutions.

--- src/week2/day11.java
+++ src/week2/day11.java
@@ -1,9 +1,6 @@
 package week2;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class day11 {
     public static void main(String[] args) {
@@ -59,6 +56,61 @@ public class day11 {
 
         for (Map.Entry<String, Integer> entry : sorted) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+
+
+        //Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+        //
+        //
+        //
+        //Example 1:
+        //
+        //Input: strs = ["eat","tea","tan","ate","nat","bat"]
+        //
+        //Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+        //
+        //Explanation:
+        //
+        //There is no string in strs that can be rearranged to form "bat".
+        //The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+        //The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+        //Example 2:
+        //
+        //Input: strs = [""]
+        //
+        //Output: [[""]]
+        //
+        //Example 3:
+        //
+        //Input: strs = ["a"]
+        //
+        //Output: [["a"]]
+        //
+        //
+        //
+        //Constraints:
+        //
+        //1 <= strs.length <= 104
+        //0 <= strs[i].length <= 100
+        //strs[i] consists of lowercase English letters.
+
+        class Solution {
+            public List<List<String>> groupAnagrams(String[] strs) {
+                HashMap<String,List<String>> mowa=new HashMap<>();
+                //now we need to create a loop for the traversing to the sting array
+                for(String m:strs){
+                    //now we need to create a character array for all the characters that have been
+                    char[] c = m.toCharArray();
+                    Arrays.sort(c);
+                    //now we need to sort the characters
+                    String sorted=new String(c);
+                    //here we place that dorted string into the hashmap
+                    mowa.putIfAbsent(sorted,new ArrayList<>());
+                    mowa.get(sorted).add(m);
+                }
+                return new ArrayList<>(mowa.values());
+            }
         }
     }
 }
