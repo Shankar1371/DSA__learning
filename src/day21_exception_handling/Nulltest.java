e13f22b0b8ea611b88035c8c30f23b3da74e0bdb
Shankar1371 <shankar.uma1371@gmail.com>
2025-10-01 22:54:34 -0500

    learnt how to use the exception and try catch and finally block in java and also learnt about null point exception handling along with the arithematic exeption handling

--- src/day21_exception_handling/Nulltest.java
+++ src/day21_exception_handling/Nulltest.java
@@ -9,8 +9,8 @@ package day21_exception_handling;
 // throws -- declares that a method  might throw and error
 
 
-public class Dividetest {
-    public static void main(String[] args){
+class Dividetest {
+    public static void mn(String[] args){
         try{
             int a=10,b=0;
             int result=a/b;
@@ -25,4 +25,16 @@ public class Dividetest {
     }
 }
 
-//now
+//now we are gonna use NUll pointer handling in java
+//what is null pointer
+//null pointer is that does not point to a valid memory location or object but instead holds a special , typically zero valued and address to indicate the invalidty
+public class Nulltest{
+    public static void main(String[] args){
+        try{
+            String s=null;
+            System.out.println(s.length());
+        }catch(NullPointerException e){
+            System.out.println("Null value:"+ e.getMessage());
+        }
+    }
+}
